e633f9c048fa1d9e04e76142654fb6e4178a90d9
duds08422-max <duds08422-max@users.noreply.github.com>
2026-09-24 23:12:02 +0200

    pages: corporate product site (hartwell-labs.github.io/fortis)

--- build.rs
+++
@@ -1,7 +0,0 @@
-#![allow(clippy::expect_used)] // build scripts must panic on failure
-
-fn main() {
-    let ld_script = std::fs::canonicalize("link.ld").expect("link.ld not found");
-    println!("cargo:rustc-link-arg=-T{}", ld_script.display());
-    println!("cargo:rerun-if-changed=link.ld");
-}
